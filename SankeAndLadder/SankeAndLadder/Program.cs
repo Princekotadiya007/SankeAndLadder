da004889e51c273b274ca44920261ff6742ef895
Princekotadiya007 <princuuu77@gmail.com>
2023-02-08 12:52:12 +0530

    [prince] starting position

---
+++ SankeAndLadder/SankeAndLadder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SankeAndLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            //int diceRoll;
+            Random random = new Random();
+
+            Console.WriteLine("starting point"+position);
+        }
+    }
+}
