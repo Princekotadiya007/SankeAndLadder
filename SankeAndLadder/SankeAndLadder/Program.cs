e6c91e9fa3390d975b77c07e27ec150bb1a3e3d7
Princekotadiya007 <princuuu77@gmail.com>
2023-02-08 13:36:14 +0530

    [prince] winning position 100

--- SankeAndLadder/SankeAndLadder/Program.cs
+++ SankeAndLadder/SankeAndLadder/Program.cs
@@ -12,30 +12,49 @@ namespace SankeAndLadder
         {
             int position = 0;
             int diceRoll;
-            //int option = 0
+            int option;
+            Random random = new Random();
 
-            Random random = new Random();  //Creating object random
-            diceRoll = random.Next(1, 7); // //Using random method
-
-            Console.WriteLine("starting point{0} and random number{1}:"+position,diceRoll);
-
-            Random random1 = new Random(); //Creating object random
-            int option = random1.Next(1, 4);  //Using random method
-
-            switch (option)
+            while (position < 100)
             {
-                case 1:
-                    Console.WriteLine("No Play. You stay in the same position.");
-                    break;
-                case 2:
-                    Console.WriteLine("Ladder! You move ahead by " + diceRoll + " positions.");
-                    position += diceRoll;
-                    break;
-                case 3:
-                    Console.WriteLine("Snake! You move back by " + diceRoll + " positions.");
+                diceRoll = random.Next(1, 7);
+                Console.WriteLine("You rolled a " + diceRoll);
+                option = random.Next(1, 4);
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("No Play. You stay in the same position.");
+                        break;
+                    case 2:
+                        Console.WriteLine("Ladder! You move ahead by " + diceRoll + " positions.");
+                        position += diceRoll;
+                        break;
+                    case 3:
+                        Console.WriteLine("Snake! You move back by " + diceRoll + " positions.");
+                        position -= diceRoll;
+                        break;
+                }
+
+                if (position < 0)
+                {
+                    Console.WriteLine("You went below position 0. Restarting from position 0.");
+                    position = 0;
+                }
+                else if (position > 100)
+                {
+                    Console.WriteLine("You went above position 100. Staying at position " + (position - diceRoll));
                     position -= diceRoll;
-                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Your new position is " + position);
+                }
             }
+
+            Console.WriteLine("You won the game! You are at position 100");
+            Console.ReadLine();
+
         }
     }
 }
